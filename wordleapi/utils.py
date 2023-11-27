92e8ad8f34a86f053803066d7621ca6bece767b7
frechousky <alexandre.freche@gmail.com>
2023-11-27 11:12:58 +0100

    remove now function (not used)

--- wordleapi/utils.py
+++ wordleapi/utils.py
@@ -4,21 +4,13 @@ import random
 import pytz
 
 
-def now() -> datetime.datetime:
-    """
-    Returns:
-        Current date and time in Paris (France) as datetime.datetime object
-    """
-    return datetime.datetime.now(pytz.timezone("Europe/Paris"))
-
-
 def now_yyyymmdd() -> str:
     """
     Returns:
         Current date in Paris (France) as "yyyyMMdd" string
         (e.g. on 2023/08/07 returns "20230807")
     """
-    return now().strftime("%Y%m%d")
+    return datetime.datetime.now(pytz.timezone("Europe/Paris")).strftime("%Y%m%d")
 
 
 def strip_lower(s: str) -> str | None:
